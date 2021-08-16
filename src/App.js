ec1c5e325af0b869616ecd96eb00ea1e82755b88
new name <new email>
2021-08-17 00:49:27 +0600

    final

--- src/App.js
+++ src/App.js
@@ -5,21 +5,45 @@ import Country from "./Components/Country/Country";
 function App() {
   const [countries, setCountries] = useState([]);
 
-  useEffect(() =>{
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(data => data.json())
-    .then(data => setCountries(data))
-    .catch(error => error)
-  },[] )
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((data) => data.json())
+      .then((data) => setCountries(data))
+      .catch((error) => error);
+  }, []);
+  const [cart, setCart] = useState([])
+  const [name, setName] = useState([])
+  const [population, setPopulation] = useState([])
+
+  const handleAddCountry = (country) => {
+      const newName = country.name;
+      const newPopulation = country.population;
+      const newCart = [...cart, country] 
+      setPopulation(newPopulation)
+      setCart(newCart)
+      setName(newName)
+    }
+
+    const totalPopulation = cart.reduce((total, cart) => total+cart.population,0)
+
+
 
   return (
+    <>
+    <div className="navbar">
+      <h4>Country Added: {cart.length}</h4>
+      <p>Last Added: {name} {population} </p>
+      <h4>Total population: {totalPopulation} </h4>
+    </div>
+    
     <div className="App">
       <ul>
-        {
-          countries.map(country => <Country country={country} key={country.alpha3Code}></Country>)
-        }
+        {countries.map((country) => (
+          <Country country={country} cart={cart} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>
+        ))}
       </ul>
     </div>
+    </>
   );
 }
 
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
-  const { name, area, flag, region } = country;
+const Country = ({ country, handleAddCountry, cart}) => {
+  const { name, area, flag, region, population } = country;
+
   return (
+      <>
     <div className="country">
       <div className="details">
         <h4>Name: {name}</h4>
+        <h5>Population: {population}</h5>
         <h5>Area: {area}</h5>
         <h5>Region: {region}</h5>
+        <button onClick={() => handleAddCountry(country)}>Add Population</button>
       </div>
       <div>
         <img src={flag} alt="" />
       </div>
     </div>
+    </>
   );
 };
 
